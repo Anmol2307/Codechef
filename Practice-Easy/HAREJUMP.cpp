bd56fcc3a2d505061bded714f2528396e74bbe54
Anmol <garganmol1993@gmail.com>
2014-09-11 15:22:43 +0530

    HAREJUMP AC

--- Practice-Easy/HAREJUMP.cpp
+++ Practice-Easy/HAREJUMP.cpp
@@ -11,49 +11,64 @@ inline void inp(int &n ) {//fast input function
     n=n*sign;
 }
 
-typedef unsigned long long llu;
-#define MOD 1000000007
+typedef long long ll;
+typedef vector < ll > row;
+typedef vector < row > matrix;
 
-int k;
-vector <llu> vec;
-map<llu,llu> mymap;
+ll MOD = (ll) 1e9 + 7;
 
+void clear (matrix & A) {
+  for (size_t i = 0; i < A.size (); i++)
+    for (size_t j = 0; j < A[i].size (); j++)
+      A[i][j] = 0;
+}
 
-llu findWays(llu n) {
-	if (n == 0) return 0;
-	if (mymap.find(n) != mymap.end()) return mymap[n];
-	if (k == 1 && n % vec[0] == 0) return 1;
-	else if (k == 1) return 0;
-	
-	llu ans = 0;
-	sort(vec.begin(), vec.end());
-	for (int i = 0; i < k; i++) {
-		if (vec[i] < n) ans = (ans + findWays(n-vec[i]))%MOD;
-		if (vec[i] == n) ans = (ans + 1)%MOD;
-	}
-	mymap.insert(pair<llu,llu>(n,ans));
-	return ans;
+matrix mul (const matrix & A, const matrix & B) {
+  matrix C = A;
+  clear (C);
+  for (size_t i = 0; i < C.size (); i++)
+    for (size_t j = 0; j < C[i].size (); j++)
+      for (size_t k = 0; k < A[i].size (); k++)
+  C[i][j] = (C[i][j] + A[i][k] * B[k][j]) % MOD;
+  return C;
+}
+
+matrix pow (const matrix & A, ll p) {
+  if (p == 0) {
+      matrix C = A;
+      clear (C);
+      for (size_t i = 0; i < C.size (); i++)
+        C[i][i] = 1;
+      return C;
+  }
+
+  matrix C = pow (A, p / 2);
+  C = mul (C, C);
+  if (p & 1)
+    C = mul (C, A);
+  return C;
+}
+
+void solve () {
+  ll L;
+  scanf("%lld",&L);
+  const int nn = 16;
+  matrix Z = matrix (nn, row (nn, 0));
+  matrix A = Z;
+  for (int i = 1; i < nn; i++)
+    A[i][i - 1] = 1;
+  int k, l;
+  inp(k);
+  for (int i = 0; i < k; i++) {
+      inp(l);
+      A[0][l - 1]++;
+  }
+  matrix B = pow (A, L);
+  B[0][0] = (B[0][0]*2)%MOD;
+  printf("%lld\n",B[0][0]);
 }
 
 int main () {
-
-	int t;
-	inp(t);
-
-	while (t--) {
-		vec.clear();
-		mymap.clear();
-
-		llu n;
-		scanf("%llu",&n);
-		inp(k);
-		
-		for (int i = 0; i < k; i++) {
-			llu x;
-			scanf("%llu",&x);
-			vec.push_back(x);
-		}
-
-		printf("%llu\n",findWays(n));
-	}
+  solve ();
+  return 0;
 }
